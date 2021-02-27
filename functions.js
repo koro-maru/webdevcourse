54c7d0681e1e82df80d0b3c280c5788a9d98823a
EMILY SOTO <sotoemily03@gmail.com>
2021-02-26 21:28:14 -0500

    functions

---
+++ functions.js
@@ -0,0 +1,29 @@
+
+
+const addition = (num1, num2) =>{ 
+    return num1 + num2;
+}
+
+console.log(addition(num1, num2));
+
+function subtraction(num1, num2){
+    return num2 - num1;
+}
+
+console.log(subtraction(30,31));
+
+class Dog{
+    constructor(breed, name, age, temperament){
+        this.breed = breed;
+        this.name = name;
+        this.age = age;
+        this.temperament = temperament;
+    }
+
+    dogInfo(){
+        console.log(`${this.name} the dog is a ${this.age} year old ${this.breed} with an ${this.temperament} temperament.`)
+    };
+}
+
+const luke = new Dog("Yorkie", "Luke", 5, "energetic");
+console.log(luke.dogInfo());
